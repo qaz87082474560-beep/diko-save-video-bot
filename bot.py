a652faf9354cdc9cb50dd523a26ea680b6bd693c
qaz87082474560-beep <qaz87082474560@gmail.com>
2026-05-07 16:14:30 +0500

    Enhance video download options in download_video function
    Updated video download options to include better format and merging settings.

--- bot.py
+++ bot.py
@@ -24,14 +24,15 @@ mode = {}
 # VIDEO DOWNLOAD
 def download_video(url):
 
-    ydl_opts = {
-        'outtmpl': 'video.%(ext)s',
-        'format': 'best',
-        'quiet': True,
-        'noplaylist': True,
-        'cookiefile': 'cookies.txt'
-    }
-
+   ydl_opts = {
+    'outtmpl': 'video.%(ext)s',
+    'format': 'bestvideo+bestaudio/best',
+    'merge_output_format': 'mp4',
+    'quiet': True,
+    'noplaylist': True,
+    'cookiefile': 'cookies.txt',
+    'nocheckcertificate': True
+}
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(url, download=True)
         return ydl.prepare_filename(info)
